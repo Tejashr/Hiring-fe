eab50df538825be6a5d44a0d708fda991b61df89
Tejashr <tejasteju11@gmail.com>
2021-06-21 20:37:43 +0530

    white button

--- src/nav.js
+++ src/nav.js
@@ -15,7 +15,7 @@ function Nav() {
         <>
             <nav class="navbar navbar-expand-lg navbar-light bgcolor">
                 <a class="navbar-brand text-light"><FontAwesomeIcon icon={faScrewdriver}></FontAwesomeIcon> HIRING</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
